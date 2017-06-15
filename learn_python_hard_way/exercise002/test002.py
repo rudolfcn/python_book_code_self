bfc5e3dc2d96027e88c377be2b1f7ae952cbd7da
cym <chenyongming@angonsoft.com>
2017-06-15 09:50:05 +0800

    remove old folder.

--- learn_python_hard_way/exercise002/test002.py
+++
@@ -1,9 +0,0 @@
-# A comment.
-
-print "I can have code like this." # add comment after sentence
-
-#use a comment to disable or comment out a piece of code.
-
-print 'This will run.'
-
-#print "This won't run."
